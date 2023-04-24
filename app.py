a5e198141ee9b9f475f1569970ccb39452a26451
mozoezomoe <dres2112@gmail.com>
2023-04-24 09:39:44 +0200

    new adds

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, send_file, render_template
 import pandas as pd
 import pickle
-import joblib
 import io
 import csv
 
@@ -20,18 +19,54 @@ def predict():
     file = request.files['file']
 
     # Load the model
-    if selected_model == 'mlp_full':
+    if selected_model == 'mlp_full_plasm_ant':
         with open('models/mlp_full_plasmAnt.pkl', 'rb') as f:
             model = pickle.load(f)
-    elif selected_model == 'mlp_met':
+    elif selected_model == 'mlp_met_plasm_ant':
         with open('models/mlp_met_plasmAnt.pkl', 'rb') as f:
             model = pickle.load(f)
-    elif selected_model == 'rf_full':
+    elif selected_model == 'rf_full_plasm_ant':
         with open('models/rf_full_plasmAnt.pkl', 'rb') as f:
             model = pickle.load(f)
-    elif selected_model == 'rf_met':
+    elif selected_model == 'rf_met_plasm_ant':
         with open('models/rf_met_plasmAnt.pkl', 'rb') as f:
             model = pickle.load(f)
+    elif selected_model == 'mlp_full_plasm_flav':
+        with open('models/mlp_full_plasmFlav.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'mlp_met_plasm_flav':
+        with open('models/mlp_met_plasmFlav.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'rf_full_plasm_flav':
+        with open('models/rf_full_plasmFlav.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'rf_met_plasm_flav':
+        with open('models/rf_met_plasmFlav.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'mlp_full_urine_ant':
+        with open('models/mlp_full_urineAnt.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'mlp_met_urine_ant':
+        with open('models/mlp_met_urineAnt.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'rf_full_urine_ant':
+        with open('models/rf_full_urineAnt.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'rf_met_urine_ant':
+        with open('models/rf_met_urineAnt.pkl', 'rb') as f:
+            model = pickle.load(f)                
+    elif selected_model == 'mlp_full_urine_flav':
+        with open('models/mlp_full_urineFlav.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'mlp_met_urine_flav':
+        with open('models/mlp_met_urineFlav.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'rf_full_urine_flav':
+        with open('models/rf_full_urineFlav.pkl', 'rb') as f:
+            model = pickle.load(f)
+    elif selected_model == 'rf_met_urine_flav':
+        with open('models/rf_met_urineFlav.pkl', 'rb') as f:
+            model = pickle.load(f)    
     else:
         return 'Error: Invalid model selection'
 
@@ -53,75 +88,6 @@ def predict():
         attachment_filename='predictions.csv',
         as_attachment=True
     )
-
-@app.route('/show_data',  methods=("POST", "GET"))
-def showData():
-    # Get the selected model
-    selected_model = request.form['model']
-
-    # Get the uploaded file
-    file = request.files['file']
-
-    # Load the model
-    if selected_model == 'mlp_full':
-        with open('models/mlp_full_plasmAnt.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'mlp_met':
-        with open('models/mlp_met_plasmAnt.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_full':
-        with open('models/rf_full_plasmAnt.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_met':
-        with open('models/rf_met_plasmAnt.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'mlp_full_plasmFlav':
-        with open('models/mlp_full_plasmFlav.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'mlp_met_plasmFlav':
-        with open('models/mlp_met_plasmFlav.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_full_plasmFlav':
-        with open('models/rf_full_plasmFlav.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_met_plasmFlav':
-        with open('models/rf_met_plasmFlav.pkl', 'rb') as f:
-            model = pickle.load(f)
-    if selected_model == 'mlp_full_urineAnt':
-        with open('models/mlp_full_urineAnt.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'mlp_met_urineAnt':
-        with open('models/mlp_met_urineAnt.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_full_urineAnt':
-        with open('models/rf_full_urineAnt.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_met_urineAnt':
-        with open('models/rf_met_urineAnt.pkl', 'rb') as f:
-            model = pickle.load(f)                
-    elif selected_model == 'mlp_full_urineFlav':
-        with open('models/mlp_full_urineFlav.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'mlp_met_urineFlav':
-        with open('models/mlp_met_urineFlav.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_full_urineFlav':
-        with open('models/rf_full_urineFlav.pkl', 'rb') as f:
-            model = pickle.load(f)
-    elif selected_model == 'rf_met_urineFlav':
-        with open('models/rf_met_urineFlav.pkl', 'rb') as f:
-            model = pickle.load(f)    
-    else:
-        return 'Error: Invalid model selection'
-
-    # Make predictions
-    df = pd.read_csv(file)
-    predictions = model.predict(df)
-
-    # Convert pandas dataframe to html table flask
-    df_html = predictions.to_html()
-    return render_template('index.html', data=df_html)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
